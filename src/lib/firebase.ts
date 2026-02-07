76c0b548bbfdd598d03eaa61a558a18db01cd752
saurav kumar <saurav316kumar@gmail.com>
2026-02-07 06:10:27 +0000

    then should i do it now?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,8 +7,8 @@ const firebaseConfig = {
   appId: "1:819351559125:web:c2a963e94d715d551fbb5c",
   apiKey: "AIzaSyB2fQUTz-UiV8AXxxlUcqgurtKO9504qBU",
   authDomain: "studio-163857673-d7a9f.firebaseapp.com",
-  storageBucket: "studio-163857673-d7a9f.appspot.com",
   messagingSenderId: "819351559125",
+  measurementId: ""
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
