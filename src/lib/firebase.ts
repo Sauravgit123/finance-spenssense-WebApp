ac480ebae9218ba09f376979f85d7af28652ea4a
saurav kumar <saurav316kumar@gmail.com>
2026-02-07 16:23:19 +0000

    wait does it have proper backend and all

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "studio-163857673-d7a9f",
-  "appId": "1:819351559125:web:c2a963e94d715d551fbb5c",
-  "apiKey": "AIzaSyB2fQUTz-UiV8AXxxlUcqgurtKO9504qBU",
-  "authDomain": "studio-163857673-d7a9f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "819351559125"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
