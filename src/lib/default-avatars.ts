15471fdda894012ca742abc378e844b52cf1c081
saurav kumar <saurav316kumar@gmail.com>
2026-02-08 18:06:48 +0000

    i have SAID AI FACE AVATARS DEAFULT NO ALTU FALTU IMAGES

--- src/lib/default-avatars.ts
+++ src/lib/default-avatars.ts
@@ -1,9 +1,9 @@
 
 export const defaultAvatars: string[] = [
-  'https://picsum.photos/seed/male1/200',
-  'https://picsum.photos/seed/female1/200',
-  'https://picsum.photos/seed/male2/200',
-  'https://picsum.photos/seed/female2/200',
-  'https://picsum.photos/seed/male3/200',
-  'https://picsum.photos/seed/female3/200',
+  'https://avatar.iran.liara.run/public/boy',
+  'https://avatar.iran.liara.run/public/girl',
+  'https://avatar.iran.liara.run/public/4',
+  'https://avatar.iran.liara.run/public/33',
+  'https://avatar.iran.liara.run/public/50',
+  'https://avatar.iran.liara.run/public/99',
 ];
